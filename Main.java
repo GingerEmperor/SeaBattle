7e5cae00182fb6464e8e88957883d43e794ebf67
GingerEmperor <maks.kolesnik98@mail.ru>
2018-10-30 19:52:55 +0300

    Ввод рвзмера и координат корабля с клавиатуры

--- Main.java
+++ Main.java
@@ -6,17 +6,21 @@ import java.util.Set;
 public class Main {
 
     public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    System.out.println("Enter the cordinates of the Ship's tail");
+    int tailOfShip=in.nextInt();//Ввод координат хвоста
+    System.out.println("Enter the size of the ship");
+    int sizeOfShip=in.nextInt();//Ввод размера корабля
     int record=0;
-	int[] locOfShip={2,3,4};
-	int tailOsHhip=locOfShip[0];
+	int[] locOfShip=new int[sizeOfShip];
 	Ships Ship1=new Ships();
-	Ship1.SetLoc(locOfShip,tailOsHhip);
-	Scanner in=new Scanner(System.in);
-	while(!(Ship1.G())){
+	Ship1.SetLoc(locOfShip,tailOfShip);
+
+	while(!(Ship1.G())){//пока игра не закончена
 	    record++;
-	    System.out.println("Shoot:  ");
+	    System.out.println("Shoot:  ");//ввод выстрела
 	    int myS=in.nextInt();
-	    Ship1.IsDead(myS);
+	    Ship1.IsDead(myS,sizeOfShip);
 
     }
     System.out.println("Your record is "+record);
@@ -24,8 +28,8 @@ public class Main {
 
 
     public static class Ships{
-        private int[] location = new int[10];
-        private String checkYouSelf;
+        private int[] location = new int[100]; // Карта на 100 ячеек
+        private String checkYouSelf; //состояние корабля
         private int DeadShoots=0;
         private boolean GameIsOver=false;
 
@@ -41,10 +45,10 @@ public class Main {
         }
 
 
-        public void IsDead(int Shot){
-            if(location[Shot]==1){
+        public void IsDead(int Shot, int size){
+            if(location[Shot]==1){//Если в ячейке есть корабль
                 DeadShoots++;
-                if(DeadShoots==3) {
+                if(DeadShoots==size) {//Если кол-во попадпний == размеру корабля
                     SetCheck("Убил");
                     GameIsOver = true;
                 }
